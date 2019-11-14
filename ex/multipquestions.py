3409b09cb0bc63ebb77c79c464a6163289c9f07f
Jukka-Pekka Kerkkänen <kerkkaju@teacher.eursc.eu>
2019-11-14 12:00:26 +0100

    edit multiex

--- ex/multipquestions.py
+++ ex/multipquestions.py
@@ -1,17 +1,38 @@
 import random
-def makequestion(ceiling):
-    q = str(random.randint(0, ceiling))+" x "+str(random.randint(0, ceiling))
-    return q
 
+def make_question(int1, int2):
+    question = str(i1)+" x "+str(i2)+" = "
+    return question
+
+def show_results(questions, answers, right_answers):
+    result = ""
+    correct = False
+    for i in range(len(questions)):
+        if(answers[i]==right_answers[i]):
+            feedback = "The answer was correct"
+        else:
+            feedback = "The answer was wrong"
+
+        result += "Q"+str(i+1)+") "+questions[i]+ str(right_answers[i])+". "
+        result += "User answered "+str(answers[i])+". "+feedback+"\n"
+    return result
+
+# Main:
 questions = list()
-answer = list()
+right_answers = list()
+answers = list()
 
 print("How many questions?")
-numb = input()
+numb = int(input())
 print("Give the max number")
-max = input()
+ceiling = int(input())
 
 for i in range(numb):
-    q = makequestion(max)
-    print
-    questions.append
+    i1 = random.randint(1, ceiling)
+    i2 = random.randint(1, ceiling)
+    questions.append(make_question(i1,i2))
+    right_answers.append(i1*i2)
+    print(questions[i])
+    answers.append(int(input()))
+
+print(show_results(questions, answers, right_answers))
