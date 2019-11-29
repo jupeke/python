2a4606f5a88b0f153be800780df8b9c27f37a032
Jukka-Pekka Kerkkänen <kerkkaju@teacher.eursc.eu>
2019-11-29 15:41:23 +0100

    edit multiplications_test_oop.py

--- ex/multiplication_test_oop.py
+++ ex/multiplication_test_oop.py
@@ -8,7 +8,14 @@ class Teacher:
         self.ceiling = 10    # Default
         self.number_of_questions = 5 # Default
 
+    def test(self):
+        # Empties old questions:
+        self.questions = list()
+        for i in range(self.number_of_questions):
+            self.ask_question(self.ceiling)
+
     def ask_question(self, ceiling):
+        print()
         q = Question(ceiling)
         print(q.output())
         q.save_answer()
@@ -17,15 +24,16 @@ class Teacher:
     def show_all_results(self):
         result = ""
         correct = False
+        counter = 1
         for q in self.questions:
             if(q.answer_is_ok()):
                 feedback = "The answer was correct."
             else:
                 feedback = "The answer was wrong."
-
-            result += "Q"+str(i+1)+") "+q.output()+ str(q.correct_answer)+". "
+            result += "Q"+str(counter)+") "+q.output()+ str(q.correct_answer)+". "
             result += "User answered "+ str(q.user_answer)+". "+feedback+"\n"
-        return result
+        print()
+        print(result)
 
     def show_total(self):
         points = 0
@@ -33,16 +41,25 @@ class Teacher:
             if(q.answer_is_ok()):
                 points += 1
         respons = "Total: "+str(points)+"/"+str(len(self.questions))
-        return respons
+        print()
+        print(respons)
 
     def say_hello(self):
+        print()
         print("Hello! My name is {} {} and I will ask a few questions.".\
             format(self.title, self.name))
-    def ask_ceiling(self):
-        print("Give the biggest possible number")
 
-    def ask_numb_of_questions(self):
+    # Asks for a ceiling and stores it to self.ceiling
+    def ask_for_ceiling(self):
+        print()
+        print("Give the biggest possible number")
+        self.ceiling = int(input())
+
+    # Asks for a number of questions and stores it to self.number_of_questions
+    def ask_for_numb_of_questions(self):
+        print()
         print("How many questions do you want to hear?")
+        self.number_of_questions = int(input())
 
 
 class Question:
@@ -67,19 +84,13 @@ class Question:
 end = False
 prof = Teacher("Kerkkänen", "Mr. ")
 while (end == False):
-    print()
     prof.say_hello()
-    print("How many questions do you want to hear?")
-    numb = int(input())
-    print("Give the biggest possible number")
-    ceiling = int(input())
-    print()
-    for i in range(numb):
-        prof.ask_question(ceiling)
-    print()
-    print(prof.show_all_results())
-    print(prof.show_total())
-
+    prof.ask_for_numb_of_questions()
+    prof.ask_for_ceiling()
+    prof.test()
+    prof.show_all_results()
+    prof.show_total()
+    # If user wants to go on...
     onemore = input("Another test (yes/no)? ")
     if (onemore != "yes"):
         end = True
