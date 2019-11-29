8c9553c840cd6482965824aca692295144a2e30b
Jukka-Pekka Kerkkänen <kerkkaju@teacher.eursc.eu>
2019-11-29 10:54:33 +0100

    oop multiptest working

--- ex/multiplication_test_oop.py
+++ ex/multiplication_test_oop.py
@@ -8,67 +8,68 @@ class Teacher:
 
     def ask_question(self, ceiling):
         q = Question(ceiling)
+        print(q.output())
+        q.save_answer()
+        self.questions.append(q)
 
-    def show_all_results(self, questions):
+    def show_all_results(self):
         result = ""
         correct = False
-        for i in range(len(questions)):
-            if(answers[i]==right_answers[i]):
+        for q in self.questions:
+            if(q.answer_is_ok()):
                 feedback = "The answer was correct."
             else:
                 feedback = "The answer was wrong."
 
-            result += "Q"+str(i+1)+") "+questions[i]+ str(right_answers[i])+". "
-            result += "User answered "+str(answers[i])+". "+feedback+"\n"
+            result += "Q"+str(i+1)+") "+q.output()+ str(q.correct_answer)+". "
+            result += "User answered "+ str(q.user_answer)+". "+feedback+"\n"
         return result
 
     def show_total(self):
         points = 0
-        for i in range(len(answers)):
-            if(answers[i]==right_answers[i]):
+        for q in self.questions:
+            if(q.answer_is_ok()):
                 points += 1
-        respons = "Total: "+str(points)+"/"+str(len(right_answers))
+        respons = "Total: "+str(points)+"/"+str(len(self.questions))
         return respons
 
+    
 
 class Question:
     def __init__(self, ceiling):
         self.number1 = random.randint(1, ceiling)
         self.number2 = random.randint(1, ceiling)
         self.user_answer = ""
+        self.correct_answer = self.number1 * self.number2
 
     def output(self):
         q = str(self.number1)+" x "+str(self.number2)+" = "
         return q
     def save_answer(self):
         self.user_answer = int(input())
-
-
-
-
+    def answer_is_ok(self):
+        if(self.user_answer == self.correct_answer):
+            return True
+        else:
+            return False
 
 # Main:
 end = False
 prof = Teacher("Kerkkänen", "Mr. ")
 while (end == False):
-
     print()
-    print("How many questions?")
+    print("Hello! My name is ",prof.title,prof.name,
+            "and I will ask a few questions.")
+    print("How many questions do you want to hear?")
     numb = int(input())
-    print("Give the max number")
+    print("Give the biggest possible number")
     ceiling = int(input())
     print()
-
     for i in range(numb):
-
-        questions.append(make_question(i1,i2))
-        right_answers.append(i1*i2)
-        print("Question "+str(i+1)+"/"+str(numb)+": "+questions[i]+"?")
-        answers.append(int(input()))
-
+        prof.ask_question(ceiling)
     print()
-    print(show_results(questions, answers, right_answers))
-    print(show_total(answers, right_answers))
+    print(prof.show_all_results())
+    print(prof.show_total())
 
     onemore = input("Another test (yes/no)? ")
     if (onemore != "yes"):
