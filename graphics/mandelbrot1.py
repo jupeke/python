f2becacd83965dd424853d6622d3bb5db39bff11
Jukka-Pekka Kerkkänen <kerkkaju@teacher.eursc.eu>
2021-09-24 14:24:38 +0200

    mandelbrot: coordinate axes ok

--- graphics/mandelbrot1.py
+++ graphics/mandelbrot1.py
@@ -8,19 +8,37 @@ class MainProgram():
         self.red = self.MAX_COLOR
         self.green = 0
         self.blue = 0
-        self.win_width = 800
-        self.win_height = 800
+        self.win_width = 1000
+        self.win_height = 1000
         self.window = GraphWin('Mandelbrot', self.win_width, self.win_height) # give title and dimensions
         self.window.setBackground('white') # background color
-        self.message = Text(Point(self.win_width/2, 20), 'Click to Exit')
-        self.message.draw(self.window)
+        self.origin_x = self.win_width/2
+        self.origin_y = self.win_height/2
+        #self.message = Text(Point(self.origin_x, 20), 'Click to Exit')
+        #self.message.draw(self.window)
 
     def run(self):
+        self.create_coordinates(self.window)
         self.create_points()
         self.draw_points()
+        for p in self.points:
+            p.get_mb_testpoints()
+            p.draw_testpoints()
+
+    def create_coordinates(self, win):
+        y_axe = Line(Point(self.origin_x,0), Point(self.origin_x,self.win_height))
+        y_axe.setArrow("first")
+        y_axe.draw(win)
+        x_axe = Line(Point(0,self.origin_y), Point(self.win_width,self.origin_y))
+        x_axe.setArrow("last")
+        x_axe.draw(win)
+        label_x = Text(Point(self.win_width-30, self.origin_y+10), 'x')
+        label_x.draw(win)
+        label_y = Text(Point(self.origin_x+10, 20), 'y')
+        label_y.draw(win)
 
     def create_points(self):
-        p = ComplexPoint(complex(100,100))
+        p = ComplexPoint(complex(0,1),self.window)
         self.points.append(p)
 
     def draw_points(self):
@@ -28,17 +46,24 @@ class MainProgram():
             p.draw_me(self.window)
 
 class ComplexPoint:
-    MB_TEST_DEPTH = 100
-    def __init__(self, cnumber):
+    MB_TEST_DEPTH = 50
+    is_mb_point = False # Is a Mandelbrot point or not?
+
+    def __init__(self, cnumber, window):
         self.cnumber = cnumber
         self.x = cnumber.real
         self.y = cnumber.imag
-        self.mb_test_points = []
+        self.window = window
+        self.mb_testpoints = []
 
     # Draw the point to the window. The origin (0,0) is at the center of the window.
     def draw_me(self, win):
         p = self.get_point_object(win)
         p.draw(win)
+
+    def draw_testpoints(self):
+        for p in self.mb_testpoints:
+            p.draw_me(self.window)
 
     def get_point_object(self, win):
         x_corrected = self.x+win.getWidth()/2
@@ -46,18 +71,24 @@ class ComplexPoint:
         p = Point(x_corrected, y_corrected)
         return p
 
+    # Return the modulus this complex number.
     def modulus(self):
-        r = self.x jakta
+        r = (self.x ** 2 + self.y ** 2) ** 0.5
+        return r
 
-    def is_mandelbrot_point(self):
-        jkljöj
-    def get_mb_test_points(self):
+    def get_mb_testpoints(self):
+        self.is_mandelbrot_point = True # Default first
         testpoint = self.cnumber
-        for i in range(MB_TEST_DEPTH):
-            self.mb_test_points.append(testpoint)
+        for i in range(self.MB_TEST_DEPTH):
+            cpoint = ComplexPoint(testpoint, self.window)
+            self.mb_testpoints.append(cpoint)
+
+            # Test if is a Mandelbrot number:
+            if self.is_mandelbrot_point and self.modulus() > 2:
+                self.is_mandelbrot_point = False
 
             # Counts the next one:
-            testpoint = testpoint*testpoint + testpoint
+            testpoint = testpoint * testpoint + testpoint
 
 
 main = MainProgram()
