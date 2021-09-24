a3b78c0c7ec9121b2013be11048821891a51e502
Jukka-Pekka Kerkkänen <kerkkaju@teacher.eursc.eu>
2021-09-24 10:55:34 +0200

    edit mandelbrot, add .gitignore

--- graphics/mandelbrot1.py
+++ graphics/mandelbrot1.py
@@ -1,25 +1,66 @@
 from graphics import *
+import cmath
 
-def main():
-    max=256
-    red=max
-    green=0
-    blue=0
+class MainProgram():
+    MAX_COLOR = 256
+    points = []
+    def __init__(self):
+        self.red = self.MAX_COLOR
+        self.green = 0
+        self.blue = 0
+        self.win_width = 800
+        self.win_height = 800
+        self.window = GraphWin('Mandelbrot', self.win_width, self.win_height) # give title and dimensions
+        self.window.setBackground('white') # background color
+        self.message = Text(Point(self.win_width/2, 20), 'Click to Exit')
+        self.message.draw(self.window)
+
+    def run(self):
+        self.create_points()
+        self.draw_points()
+
+    def create_points(self):
+        p = ComplexPoint(complex(100,100))
+        self.points.append(p)
+
+    def draw_points(self):
+        for p in self.points:
+            p.draw_me(self.window)
+
+class ComplexPoint:
+    MB_TEST_DEPTH = 100
+    def __init__(self, cnumber):
+        self.cnumber = cnumber
+        self.x = cnumber.real
+        self.y = cnumber.imag
+        self.mb_test_points = []
+
+    # Draw the point to the window. The origin (0,0) is at the center of the window.
+    def draw_me(self, win):
+        p = self.get_point_object(win)
+        p.draw(win)
+
+    def get_point_object(self, win):
+        x_corrected = self.x+win.getWidth()/2
+        y_corrected = self.y+win.getWidth()/2
+        p = Point(x_corrected, y_corrected)
+        return p
+
+    def modulus(self):
+        r = self.x jakta
+
+    def is_mandelbrot_point(self):
+        jkljöj
+    def get_mb_test_points(self):
+        testpoint = self.cnumber
+        for i in range(MB_TEST_DEPTH):
+            self.mb_test_points.append(testpoint)
+
+            # Counts the next one:
+            testpoint = testpoint*testpoint + testpoint
 
 
-    win_width = 800
-    win_height = 800
-    window = GraphWin('Mandelbrot', win_width, win_height) # give title and dimensions
-    window.setBackground('white') # background color
-    x_middle=window.getWidth()/2 # get x of middle of drawing area
-    y_middle=window.getHeight()/2 # get y of middle of drawing area
-
-    p=Point(x_middle, y_middle)
-    p.draw(window)#draw the current circle
-
-    message = Text(Point(x_middle, 50), 'Click to Exit')
-    message.draw(window)
-    window.getMouse()# get mouse to click on screen to exit
-    window.close() # close the drawing window
-
-main()
+main = MainProgram()
+main.run()
+main.window.getMouse()# get mouse to click on screen to exit
+main.window.close() # close the drawing window
