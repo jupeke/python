0f2887f1b9219aa39dca5ec7519a3f45a2a8be30
Jukka-Pekka Kerkkänen <kerkkaju@teacher.eursc.eu>
2021-11-29 13:00:58 +0100

    Update red1.py

--- graphics_and_movement/move/red1.py
+++ graphics_and_movement/move/red1.py
@@ -1,19 +1,17 @@
-import sys
+import sys,time, math, random
 
 # setting path to be able to import from parent folder:
 '''sys.path.append('..graphics_and_movement')
 from graphics_and_movement.graphics import * Should work but does not...'''
 from graphics import *
-import time
-import random
 
 WINSIZE = 500
 win = GraphWin("My Test", WINSIZE, WINSIZE)
-my_object = Circle(Point(50, 50), 10)
-my_object.setFill("white")
-red = Circle(Point(int(random.random()*WINSIZE), 50), 10)
+me = Circle(Point(50, 50), 10)
+me.setFill("white")
+red = Circle(Point(int(random.random()*WINSIZE), WINSIZE), 10)
 red.setFill("red")
-my_object.draw(win)
+me.draw(win)
 red.draw(win)
 
 DELAY = 0.02
@@ -21,6 +19,22 @@ step = 3
 step_enemy = 2
 direction = "Right"
 dir_enemy = "Up"
+
+def dist_circles(circle1, circle2):
+    r1 = circle1.radius
+    cx1 = circle1.getCenter().x
+    cy1 = circle1.getCenter().y
+
+    r2 = circle2.radius
+    cx2 = circle2.getCenter().x
+    cy2 = circle2.getCenter().y
+
+
+    dist_centers = math.sqrt(pow(cx1-cx2,2)+pow(cy1-cy2,2))
+    #dist_centers_tothe2 = pow(cx1-cx2,2)+pow(cy1-cy2,2)
+    dist_final = dist_centers-(r1+r2)
+    return dist_final
+    #return dist_centers_tothe2
 
 # Moves obj one step (pxls) to direction dir
 def moveme(obj, dir, step):
@@ -47,8 +61,8 @@ while True:
         direction = k
 
     # Bounce from the walls.
-    r = my_object.radius
-    center = my_object.getCenter()
+    r = me.radius
+    center = me.getCenter()
     if center.x > WINSIZE-r:
         direction = "Left"
     elif center.x < 0+r:
@@ -67,8 +81,11 @@ while True:
     elif center_enemy.y < 0+r_red:
         dir_enemy = "Down"
 
-    moveme(my_object, direction, step)
+    moveme(me, direction, step)
     movered(red, dir_enemy, step_enemy)
+    if(dist_circles(me, red) < 0):
+        step = 0
+        step_enemy = 0
     time.sleep(DELAY)
 
 win.close()
