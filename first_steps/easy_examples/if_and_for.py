62665c1849cde3d0754aea9ca0ab6a271f6ebfb0
Jukka-Pekka Kerkkänen <35366811+jupeke@users.noreply.github.com>
2022-09-26 12:00:58 +0200

    Create if_and_for.py

---
+++ first_steps/easy_examples/if_and_for.py
@@ -0,0 +1,14 @@
+import lib
+def repeat_mess(number, message):
+    output = ""
+    for x in range(number):
+        output += str(x+1)+": "+message
+        if number > 5:
+            output += "\n"
+        else:
+            output += " "
+    return output
+
+number = input("How many times?")
+message = input("What message to repeat?")
+lib.nice(repeat_mess(int(number), message))
