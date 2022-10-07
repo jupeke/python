e12565200a235febed390b6627086cae30da21c0
Jukka-Pekka Kerkkänen <35366811+jupeke@users.noreply.github.com>
2022-10-07 14:26:49 +0200

    Create string_concat.py

---
+++ ex/easy/string_concat.py
@@ -0,0 +1,10 @@
+string1 = "John"
+number = 234
+final1 = "The name is "+string1+" and the number "+str(number)+"!"
+
+final2 = "The name is {} and the number {}!".format(string1, number)
+print()
+print(final1)
+print()
+print(final2)
+print()
