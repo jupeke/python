e91a4784f67636cb1abb8589cdcf4f448e7daaae
Jukka-Pekka Kerkkänen <35366811+jupeke@users.noreply.github.com>
2023-05-11 12:03:45 +0200

    Update chessboard_with_letters.py

--- turtle/chessboard_with_letters.py
+++ turtle/chessboard_with_letters.py
@@ -1,14 +1,21 @@
 import artist
+import math
 
 a = artist.Artist("black", "black", 1, 0)
 a.screen.bgcolor("#cccccc")
-sidelen = 80
-rowbeginwhite = True
-startx = -(4*sidelen-int(sidelen/2))
-starty = 4*sidelen-int(sidelen/2)
-letters = ["a","b","c","d","e","f","g","h"]
 size = 8
-#if (size > 8):
+factor = math.ceil(size/8)
+sidelen = int(80/factor)
+rowbeginwhite = True
+startx = -(factor*4*sidelen-int(sidelen/2))
+starty = factor*4*sidelen-int(sidelen/2)
+letters = ["a","b","c","d","e","f","g","h"]
+
+if (size > 8):
+    multiletters = letters
+    for i in range(factor):
+        multiletters += letters
+    letters = multiletters
 
 # Write the numbers and letters above and on the left
 x = startx
@@ -46,7 +53,7 @@ for i in range(size):
         a.square(True, x, y, sidelen)
         a.t.pencolor(textcol)
         a.jumpto(x, int(y-sidelen*0.1))
-        text = letters[k]+str(size-k)
+        text = letters[k]+str(size-i)
         fontsize = int(sidelen/7)
         a.t.write(text, align="center",
                   font=("Verdana",fontsize, "normal"))
