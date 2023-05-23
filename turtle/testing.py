45b865870aee77de6c0a6d433799dcf2d5a905b4
Jukka-Pekka Kerkkänen <35366811+jupeke@users.noreply.github.com>
2023-05-23 16:08:06 +0200

    Update testing.py

--- turtle/testing.py
+++ turtle/testing.py
@@ -1,12 +1,18 @@
 import turtle
 
 turtle.bgcolor("pink")
-turtle.pencolor("blue")
-turtle.fillcolor("green")
-turtle.pensize(30)
 
-turtle.begin_fill()
-turtle.circle(300)
-turtle.end_fill()
+def make_square(size, pencolor, fillcolor):
+    turtle.pencolor(pencolor)
+    turtle.fillcolor(fillcolor)
+    turtle.pensize(size)
+
+    turtle.begin_fill()
+    for i in range(4):
+        turtle.forward(200)
+        turtle.right(90)
+    turtle.end_fill()
+
+make_square(20, "red", "blue")
 
 turtle. Screen(). exitonclick()
