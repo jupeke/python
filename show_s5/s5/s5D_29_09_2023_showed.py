6df90cdd5fd02469002a92828e2ee6e02c53e80f
Jukka-Pekka Kerkkänen <35366811+jupeke@users.noreply.github.com>
2023-10-06 18:37:22 +0200

    Update s5D_29_09_2023_showed.py

--- show_s5/s5/s5D_29_09_2023_showed.py
+++ show_s5/s5/s5D_29_09_2023_showed.py
@@ -1,8 +1,10 @@
+import random
+
 #name = "Dan"
 #print("What's your name?")
 #name = input()
 #print("Hi "+name)
-
+'''
 print("Give me a number")
 # the command int is needed to convert a string into number.
 number1 = int(input())
@@ -18,3 +20,14 @@ for i in range(number2):
     result += "{} ".format(number1)
 
 print("Result: "+result)
+
+print("Want to go on? (yes/no)")
+ans = input()
+if (ans == "yes"):
+    print("Yes")
+elif (ans == "no"):
+    print("no")
+else:
+    print("I don't understand")
+'''
+print("My random number: {}".format(random.randint(0,100)))
